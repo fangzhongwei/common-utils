b828107dc251e6c98ebf2dfda137b85a6e4fb2c7
fangzhongwei <zhongwei.fang@56qq.com>
2016-11-29 20:43:37 +0800

    common judge

--- src/main/scala/com/lawsofnature/common/helper/RegHelper.scala
+++ src/main/scala/com/lawsofnature/common/helper/RegHelper.scala
@@ -1,9 +1,11 @@
 package com.lawsofnature.common.helper
 
+import scala.util.matching.Regex
+
 /**
   * Created by fangzhongwei on 2016/10/10.
   */
-object RegHelper {
+object RegHelper extends App{
   val MobileRegex = """^[1]([3][0-9]{1}|([4][7]{1})|([5][0-3|5-9]{1})|([8][0-9]{1}))[0-9]{8}$""".r
   val EmailRegex = """^(\w)+(\.\w+)*@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$""".r
 
@@ -13,5 +15,9 @@ object RegHelper {
 
   def isEmail(str: String): Boolean = {
     EmailRegex.pattern.matcher(str).matches
+  }
+
+  def isMatched(str:String , reg:String): Boolean = {
+    reg.r.pattern.matcher(str).matches()
   }
 }
