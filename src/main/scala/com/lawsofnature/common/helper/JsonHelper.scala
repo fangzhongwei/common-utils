6491c0b56c36c9d8d88788d28bab6c560b566c8c
fangzhongwei <zhongwei.fang@56qq.com>
2016-12-01 13:41:40 +0800

    json

--- src/main/scala/com/lawsofnature/common/helper/JsonHelper.scala
+++ src/main/scala/com/lawsofnature/common/helper/JsonHelper.scala
@@ -1,14 +1,18 @@
 package com.lawsofnature.common.helper
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 
 /**
   * Created by fangzhongwei on 2016/11/3.
   */
 object JsonHelper {
-  val mapper = new ObjectMapper()
+  val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
+  mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+  mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
 
   def writeValueAsString(anyRef: AnyRef): String = {
     mapper.writeValueAsString(anyRef)
