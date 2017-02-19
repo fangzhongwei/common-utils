37f4905e0bb2793dd76fd993bf4364d6648be9b0
fangzhongwei <zhongwei.fang@56qq.com>
2017-02-19 17:27:29 +0800

    mobile reg

--- src/main/scala/com/jxjxgo/common/helper/RegHelper.scala
+++ src/main/scala/com/jxjxgo/common/helper/RegHelper.scala
@@ -4,7 +4,7 @@ package com.jxjxgo.common.helper
   * Created by fangzhongwei on 2016/10/10.
   */
 object RegHelper {
-  val MobileRegex = """^[1]([3][0-9]{1}|([4][7]{1})|([5][0-3|5-9]{1})|([8][0-9]{1}))[0-9]{8}$""".r
+  val MobileRegex = """^[1]([3][0-9]{1}|([4][7]{1})|([5][0-3|5-9]{1})|([7][0135678]{1})|([8][0-9]{1}))[0-9]{8}$""".r
   val EmailRegex = """^(\w)+(\.\w+)*@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$""".r
 
   def isMobile(str: String): Boolean = {
