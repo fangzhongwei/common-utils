98e5401bf88108284fb872a8f57461672a0349ae
fangzhongwei <fangzhongwei@outlook.com>
2018-09-05 08:13:14 +0800

    jackson

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "common-utils"
 
-version := "1.4"
+version := "1.5"
 
 scalaVersion := "2.12.2"
 
--- src/main/scala/com/jxjxgo/common/helper/JsonHelper.scala
+++ src/main/scala/com/jxjxgo/common/helper/JsonHelper.scala
@@ -3,7 +3,7 @@ package com.jxjxgo.common.helper
 import java.util
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 /**
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule
 object JsonHelper {
   val mapper = new ObjectMapper()
   mapper.registerModule(DefaultScalaModule)
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
   mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
   mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
 
